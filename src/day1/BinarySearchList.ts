cded1e45c127d744661392b928c88c5c7a0528bc
ejsadiarin <ejsadiarin@gmail.com>
2023-05-11 00:08:28 +0800

    TODO: Binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0; 
+  let high = haystack.length; 
+  let midpoint = low + (high - low) / 2
+  let value = haystack[midpoint];
 
+  while (low < high) {
+      
+  }
+  
 }
